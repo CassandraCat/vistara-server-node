2ca6fc113ebcee1522caf8ba0cf8922d8eb226e2
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-06-01 10:40:48 +0800

    Update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/sts', (req, res) => {
         accessKeyId: 'LTAI5tG82K2Axqf21wqoCGAA',
         accessKeySecret: 'PybUteR2QIt87wNldi0o4B4mznBUyQ'
     })
-    // roleArn填写步骤2获取的角色ARN，例如acs:ram::175708322470****:role/ramtest。
+    // roleArn填写步骤2获取的角色ARN，例如CPUacs:ram::175708322470****:role/ramtest。
     // policy填写自定义权限策略，用于进一步限制STS临时访问凭证的权限。如果不指定Policy，则返回的STS临时访问凭证默认拥有指定角色的所有权限。
     // 临时访问凭证最后获得的权限是步骤4设置的角色权限和该Policy设置权限的交集。
     // expiration用于设置临时访问凭证有效时间单位为秒，最小值为900，最大值以当前角色设定的最大会话时间为准。本示例指定有效时间为3000秒。
